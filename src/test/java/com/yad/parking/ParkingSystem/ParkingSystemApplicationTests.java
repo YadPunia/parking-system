688d6aaeaac51dd3f50d9977ba1dd9df02563c39
YadPunia <yadpunia10@gmail.com>
2018-06-16 22:24:33 +0530

    .gitignore added

--- src/test/java/com/yad/parking/ParkingSystem/ParkingSystemApplicationTests.java
+++ src/test/java/com/yad/parking/ParkingSystem/ParkingSystemApplicationTests.java
@@ -2,25 +2,18 @@ package com.yad.parking.ParkingSystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.yad.parking.parkingsystem.enumType.ParkingStatus;
-import com.yad.parking.parkingsystem.enumType.ParkingType;
-import com.yad.parking.parkingsystem.exception.ParkingSystemException;
-import com.yad.parking.parkingsystem.request.ParkingSpaceDTO;
-import com.yad.parking.parkingsystem.service.CreateParkingSpaceTypeAndNumber;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParkingSystemApplicationTests {
-	@Autowired
-	private CreateParkingSpaceTypeAndNumber allotParkingSpaceTypeAndNumber;
+	//@Autowired
+	//private CreateParkingSpaceTypeAndNumber allotParkingSpaceTypeAndNumber;
 
 	@Test
 	public void contextLoads() {
-		ParkingSpaceDTO parkingPlaceDTO = new ParkingSpaceDTO();
+		/*ParkingSpaceDTO parkingPlaceDTO = new ParkingSpaceDTO();
 		parkingPlaceDTO.setParkingPlaceStartNumber(0);
 		parkingPlaceDTO.setParkingStatus(ParkingStatus.EMPTY);
 		parkingPlaceDTO.setTotalParkingSlotNumber(5);
@@ -30,7 +23,7 @@ public class ParkingSystemApplicationTests {
 		} catch (ParkingSystemException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 }
